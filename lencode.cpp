181f2bd2b3cc79ff37a225ab657d6c4cd4115bb8
Lachlan <lachlan.kwok@student.unsw.edu.au>
2025-10-05 19:32:30 +1100

    Finish lencode

--- lencode.cpp
+++ lencode.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <unordered_map>
 #include <bitset>
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 
@@ -18,9 +20,9 @@ using namespace std;
 // ///////////////////////////// FUNCTION HEADERS //////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-uint32_t encode(string input, uint32_t reset_freq);
-u_int32_t header(u_int32_t reset_req);
-u_int32_t toBits(u_int32_t code);
+uint32_t encode(string input_path, string output_path, uint32_t reset_freq);
+void header(u_int32_t reset_req, ofstream &out_file);
+void toBits(u_int32_t code, ofstream &out_file);
 void initDictionary(unordered_map<string, u_int32_t>& dictionary);
 
 // /////////////////////////////////////////////////////////////////////////////
@@ -30,17 +32,20 @@ int main(int argc, char **argv) {
 
     cout << argc << endl;
 
-    encode("TOBEORNOTTOBEORTOBEORNOT", 10);
+    // Use Case: ./lencode [input_path] [output_path] [N]
+    string input_path = argv[1];
+    string output_path = argv[2];
+    uint32_t reset_freq = static_cast<uint32_t>(stoul(argv[3]));
+
+    encode(input_path, output_path, reset_freq);
     return 0;
 }
 
 // Encode
 
-uint32_t encode(string input, uint32_t reset_freq) {
+uint32_t encode(string input_path, string output_path, uint32_t reset_freq) {
 
-    header(reset_freq);
-
-    cout << input << endl;
+    cout << input_path << endl;
 
     unordered_map<string, uint32_t> dictionary = {};
     initDictionary(dictionary);
@@ -49,7 +54,22 @@ uint32_t encode(string input, uint32_t reset_freq) {
     string prev = "";
     uint32_t read_bytes = 0;
 
-    for (char c : input) {
+    ifstream in_file(input_path, ios::binary);
+    if (!in_file) {
+        cerr << "Failed to open file!" << endl;
+        return 1;
+    }
+
+    ofstream out_file(output_path, ios::binary);
+    if (!out_file) {
+        cerr << "Failed to open file!" << endl;
+        return 1;
+    }
+
+    header(reset_freq, out_file);
+
+    char c;
+    while (in_file.get(c)) {
         if (dictionary.find(prev + c) != dictionary.end()) {
             prev = prev + c;
         } else {
@@ -59,7 +79,7 @@ uint32_t encode(string input, uint32_t reset_freq) {
             // Output the binary
             if (!prev.empty()) {
                 u_int32_t output = dictionary[prev];
-                toBits(output);
+                toBits(output, out_file);
             }
 
             prev = string(1, c);
@@ -70,7 +90,7 @@ uint32_t encode(string input, uint32_t reset_freq) {
         if (read_bytes == reset_freq && reset_freq != 0) {
             // Need to output remaining prev
             if (!prev.empty()) {
-                toBits(dictionary[prev]);
+                toBits(dictionary[prev], out_file);
             }
 
             initDictionary(dictionary);
@@ -82,25 +102,35 @@ uint32_t encode(string input, uint32_t reset_freq) {
 
     // Need to output remaining prev
     if (!prev.empty()) {
-        toBits(dictionary[prev]);
+        toBits(dictionary[prev], out_file);
     }
+
+    out_file.close();
 
     return 1;
 }
 
-u_int32_t header(u_int32_t reset_req) {
-    bitset<FOUR_BYTE> binary(reset_req);
-    cout << binary << endl;
+void header(u_int32_t reset_req, ofstream &out_file) {
+    
+    uint8_t b1 = ((reset_req) >> 24) & 0xFF;
+    uint8_t b2 = ((reset_req) >> 16) & 0xFF;
+    uint8_t b3 = ((reset_req) >> 8) & 0xFF;
+    uint8_t b4 = reset_req & 0xFF;
 
-    return 0;
+    out_file.put(b1);
+    out_file.put(b2);
+    out_file.put(b3);
+    out_file.put(b4);
 }
 
 // Bits
-u_int32_t toBits(u_int32_t code) {
+void toBits(u_int32_t code, ofstream &out_file) {
 
     if (code < (1u << 8)) {
-        bitset<8> binary(code);
-        cout << binary << " ";
+        uint8_t byte = code & 0x7F;
+        byte &= 0x7F;
+        out_file.put(static_cast<char>(byte));
+
     } else if (code < (1u << 14)) {
         // Append 10 - it is 2 bytes long
         uint16_t bits = code & 0x3FFF;
@@ -109,7 +139,9 @@ u_int32_t toBits(u_int32_t code) {
         // Write high byte then low byte (big-endian)
         uint8_t high = (bits >> 8) & 0xFF;
         uint8_t low  = bits & 0xFF;
-        cout << bitset<8>(high) << " " << bitset<8>(low) << " ";
+
+        out_file.put(static_cast<char>(high));
+        out_file.put(static_cast<char>(low));
     } else {
         // Append 11 - it is 3 bytes long
         uint32_t bits = code & 0x3FFFFF; // 22 bits
@@ -118,10 +150,11 @@ u_int32_t toBits(u_int32_t code) {
         uint8_t b1 = (bits >> 16) & 0xFF;
         uint8_t b2 = (bits >> 8)  & 0xFF;
         uint8_t b3 = bits & 0xFF;
-        cout << bitset<8>(b1) << " " << bitset<8>(b2) << " " << bitset<8>(b3) << " ";
-    }
 
-    return 0;
+        out_file.put(static_cast<char>(b1));
+        out_file.put(static_cast<char>(b2));
+        out_file.put(static_cast<char>(b3));
+    }
 }
 
 // Initialise dictionary
